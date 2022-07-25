2cab98702c55de3c952c65ac02f5a62de07b81f4
Фельдман Эмилий Олегович <eofeldman@0000nba0a4zmd6m.msk.mts.ru>
2022-07-25 12:56:50 +0300

    added stub benchmarks

---
+++ benchmark/__init__.py
@@ -0,0 +1,13 @@
+"""
+Benchmarks for RecTools models
+===================================================
+
+Benchmark MARS models on a dataset chosen.
+One can choose either a built-in dataset or a custom dataset.
+Benchmark calcuation includes fitting a model, evaluating recommender metrics and profiling
+time and memory required for calculations.
+
+Subpackages
+-----------
+    src - Tools for benchmark calculation
+"""
