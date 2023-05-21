3eadb9a46b84347b54aecc5e6469b1bf688825dd
Yuri Ulianov <yuli1108@mail.ru>
2023-05-21 17:42:49 +0300

    updated docstring example

--- rectools/model_selection/kfold_split.py
+++ rectools/model_selection/kfold_split.py
@@ -69,14 +69,14 @@ class KFoldSplitter(Splitter):
     ...                     filter_cold_items=False, filter_already_seen=False)
     >>> for train_ids, test_ids, _ in kfs.split(interactions):
     ...     print(train_ids, test_ids)
-    [0 1 2 5 6 7] [3 4]
+    [2 7 6 1 5 0] [3 4]
     [3 4 6 1 5 0] [2 7]
     >>>
     >>> kfs = KFoldSplitter(test_size=0.25, random_state=42, n_splits=2, filter_cold_users=True,
     ...                     filter_cold_items=True, filter_already_seen=True)
     >>> for train_ids, test_ids, _ in kfs.split(interactions):
     ...     print(train_ids, test_ids)
-    [0 1 2 5 6 7] [3 4]
+    [2 7 6 1 5 0] [3 4]
     [3 4 6 1 5 0] [2]
     """
 
