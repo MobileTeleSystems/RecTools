161d27eced0358ed3a239e0434964785853b6ca2
Daria Tikhonovich <daria.m.tikhonovich@gmail.com>
2023-10-03 23:24:20 +0300

    rewrote separate features training

--- rectools/models/implicit_als.py
+++ rectools/models/implicit_als.py
@@ -58,6 +58,9 @@ class ImplicitALSWrapperModel(VectorModel):
     def __init__(self, model: AlternatingLeastSquares, verbose: int = 0, fit_features_together: bool = False):
         super().__init__(verbose=verbose)
 
+        if fit_features_together:
+            raise NotImplementedError("We temporarily do not support fitting features together")
+
         if isinstance(model, GpuAlternatingLeastSquares) and model.factors > MAX_GPU_FACTORS:  # pragma: no cover
             raise ValueError(f"When using GPU max number of factors is {MAX_GPU_FACTORS}")
         self.model: AlternatingLeastSquares
@@ -70,13 +73,7 @@ class ImplicitALSWrapperModel(VectorModel):
         ui_csr = dataset.get_user_item_matrix(include_weights=True)
 
         if self.fit_features_together:
-            user_factors, item_factors = fit_als_with_features_together(
-                self.model,
-                ui_csr,
-                dataset.user_features,
-                dataset.item_features,
-                self.verbose,
-            )
+            raise NotImplementedError("We temporarily do not support fitting features together")
         else:
             user_factors, item_factors = fit_als_with_features_separately(
                 self.model,
@@ -164,259 +161,18 @@ def fit_als_with_features_separately(
 
 
 def _fit_paired_factors(model: AlternatingLeastSquares, xy_csr: sparse.csr_matrix, y_factors: np.ndarray) -> np.ndarray:
-    if isinstance(model, GpuAlternatingLeastSquares):  # pragma: no cover
-        paired_factors = _fit_paired_factors_on_gpu(model, xy_csr, y_factors)
-    else:
-        paired_factors = _fit_paired_factors_on_cpu(model, xy_csr, y_factors)
-    return paired_factors
-
-
-def _fit_paired_factors_on_cpu(
-    model: AlternatingLeastSquares,
-    xy_csr: sparse.csr_matrix,
-    y_factors: np.ndarray,
-) -> np.ndarray:
-    x_factors = np.zeros(shape=(xy_csr.shape[0], y_factors.shape[1]), dtype=y_factors.dtype)
-    model.solver(
-        xy_csr,
-        x_factors,
-        y_factors,
-        model.regularization,
-        model.num_threads,
+    feaures_model_params = dict(
+        factors=y_factors.shape[1],
+        regularization=model.regularization,
+        alpha=model.alpha,
+        dtype=model.dtype,
+        iterations=1,
+        random_state=model.random_state
     )
-    return x_factors
-
-
-def _fit_paired_factors_on_gpu(
-    model: AlternatingLeastSquares,
-    xy_csr: sparse.csr_matrix,
-    y_factors: np.ndarray,
-) -> np.ndarray:  # pragma: no cover
-    try:
-        from implicit.gpu import (  # pylint: disable=import-outside-toplevel
-            CuCSRMatrix,
-            CuDenseMatrix,
-            CuLeastSquaresSolver,
-        )
-    except ImportError:
-        raise RuntimeError("implicit.gpu is not available")
-
-    n_factors = y_factors.shape[1]
-    if n_factors > MAX_GPU_FACTORS:
-        raise ValueError(f"When using GPU max number of factors is {MAX_GPU_FACTORS}, here is {n_factors} factors")
-
-    x_factors = np.zeros(shape=(xy_csr.shape[0], n_factors), dtype=y_factors.dtype)
-    x_cuda = CuDenseMatrix(x_factors)
-    y_cuda = CuDenseMatrix(y_factors)
-    xy_csr_cuda = CuCSRMatrix(xy_csr)
-
-    solver = CuLeastSquaresSolver(n_factors)
-    solver.least_squares(xy_csr_cuda, x_cuda, y_cuda, model.regularization, model.cg_steps)
-
-    x_cuda.to_host(x_factors)
-    return x_factors
-
-
-def fit_als_with_features_together(
-    model: AlternatingLeastSquares,
-    ui_csr: sparse.csr_matrix,
-    user_features: tp.Optional[Features],
-    item_features: tp.Optional[Features],
-    verbose: int = 0,
-) -> tp.Tuple[np.ndarray, np.ndarray]:
-    """
-    Fit ALS model with explicit features, explicit features fit together with latent.
-
-    Parameters
-    ----------
-    model: AlternatingLeastSquares
-        Base model to fit.
-    ui_csr : sparse.csr_matrix
-        Matrix of interactions.
-    user_features : (SparseFeatures | DenseFeatures), optional
-        Explicit user features.
-    item_features : (SparseFeatures | DenseFeatures), optional
-        Explicit item features.
-    verbose : int
-         Whether to print output.
-
-    Returns
-    -------
-    user_factors : np.ndarray
-        Combined latent and explicit user factors.
-    item_factors : np.ndarray
-        Combined latent and explicit user factors.
-    """
-    n_users, n_items = ui_csr.shape
-
-    # Prepare explicit factors
-    user_explicit_factors: np.ndarray
-    if user_features is None:
-        user_explicit_factors = np.array([]).reshape((n_users, 0))
-    else:
-        user_explicit_factors = user_features.get_dense()
-    n_user_explicit_factors = user_explicit_factors.shape[1]
-
-    item_explicit_factors: np.ndarray
-    if item_features is None:
-        item_explicit_factors = np.array([]).reshape((n_items, 0))
-    else:
-        item_explicit_factors = item_features.get_dense()
-    n_item_explicit_factors = item_explicit_factors.shape[1]
-
-    # Fix number of factors
-    n_factors_all = model.factors + n_user_explicit_factors + n_item_explicit_factors
-    if isinstance(model, GpuAlternatingLeastSquares) and n_factors_all % 32:  # pragma: no cover
-        padding = 32 - n_factors_all % 32
-        warnings.warn(
-            "GPU training requires number of factors to be a multiple of 32."
-            f" Increasing factors from {n_factors_all} to {n_factors_all + padding}"
-            f" (increasing latent factors from {model.factors} to {model.factors + padding})"
-        )
-        n_latent_factors = model.factors + padding
-    else:
-        n_latent_factors = model.factors
-    n_factors_all = n_latent_factors + n_user_explicit_factors + n_item_explicit_factors
-    model.factors = n_factors_all
-
-    # Prepare latent factors
-    random_state = check_random_state(model.random_state)
-    user_latent_factors = random_state.rand(n_users, n_latent_factors) * 0.01
-    item_latent_factors = random_state.rand(n_items, n_latent_factors) * 0.01
-
-    # Prepare paired factors
-    user_factors_paired_to_items = np.zeros((n_users, n_item_explicit_factors))
-    item_factors_paired_to_users = np.zeros((n_items, n_user_explicit_factors))
-
-    # Make full factors
-    user_factors = np.hstack(
-        (
-            user_explicit_factors,
-            user_latent_factors,
-            user_factors_paired_to_items,
-        )
-    ).astype(model.dtype)
-    item_factors = np.hstack(
-        (
-            item_factors_paired_to_users,
-            item_latent_factors,
-            item_explicit_factors,
-        )
-    ).astype(model.dtype)
-
-    ui_csr = ui_csr.astype(np.float32)
     if isinstance(model, GpuAlternatingLeastSquares):  # pragma: no cover
-        _fit_combined_factors_on_gpu_inplace(
-            model,
-            ui_csr,
-            user_factors,
-            item_factors,
-            n_user_explicit_factors,
-            n_item_explicit_factors,
-            verbose,
-        )
+        features_model = GpuAlternatingLeastSquares(**feaures_model_params)
     else:
-        _fit_combined_factors_on_cpu_inplace(
-            model,
-            ui_csr,
-            user_factors,
-            item_factors,
-            n_user_explicit_factors,
-            n_item_explicit_factors,
-            verbose,
-        )
-
-    return user_factors, item_factors
-
-
-def _fit_combined_factors_on_cpu_inplace(
-    model: AlternatingLeastSquares,
-    ui_csr: sparse.csr_matrix,
-    user_factors: np.ndarray,
-    item_factors: np.ndarray,
-    n_user_explicit_factors: int,
-    n_item_explicit_factors: int,
-    verbose: int,
-) -> None:
-    n_factors = user_factors.shape[1]
-    user_explicit_factors = user_factors[:, :n_user_explicit_factors].copy()
-    item_explicit_factors = item_factors[:, n_factors - n_item_explicit_factors :].copy()
-    iu_csr = ui_csr.T.tocsr(copy=False)
-
-    for _ in tqdm(range(model.iterations), disable=verbose == 0):
-        model.solver(
-            ui_csr,
-            user_factors,
-            item_factors,
-            model.regularization,
-            model.num_threads,
-        )
-        user_factors[:, :n_user_explicit_factors] = user_explicit_factors
-
-        model.solver(
-            iu_csr,
-            item_factors,
-            user_factors,
-            model.regularization,
-            model.num_threads,
-        )
-        item_factors[:, n_factors - n_item_explicit_factors :] = item_explicit_factors
-
-
-def _fit_combined_factors_on_gpu_inplace(
-    model: AlternatingLeastSquares,
-    ui_csr: sparse.csr_matrix,
-    user_factors: np.ndarray,
-    item_factors: np.ndarray,
-    n_user_explicit_factors: int,
-    n_item_explicit_factors: int,
-    verbose: int,
-) -> None:  # pragma: no cover
-    try:
-        from implicit.gpu import (  # pylint: disable=import-outside-toplevel
-            CuCSRMatrix,
-            CuDenseMatrix,
-            CuLeastSquaresSolver,
-        )
-    except ImportError:
-        raise RuntimeError("implicit.cuda is not available")
-
-    n_factors = user_factors.shape[1]
-    if n_factors > MAX_GPU_FACTORS:
-        raise ValueError(f"When using GPU max number of factors is {MAX_GPU_FACTORS}, here is {n_factors} factors")
-
-    user_explicit_factors = user_factors[:, :n_user_explicit_factors].copy()
-    item_explicit_factors = item_factors[:, n_factors - n_item_explicit_factors :].copy()
-    iu_csr = ui_csr.T.tocsr(copy=False)
-
-    iu_csr_cuda = CuCSRMatrix(iu_csr)
-    ui_csr_cuda = CuCSRMatrix(ui_csr)
-    user_factors_cuda = CuDenseMatrix(user_factors)
-    item_factors_cuda = CuDenseMatrix(item_factors)
-    solver = CuLeastSquaresSolver(n_factors)
-
-    for _ in tqdm(range(model.iterations), disable=verbose == 0):
-        solver.least_squares(
-            ui_csr_cuda,
-            user_factors_cuda,
-            item_factors_cuda,
-            model.regularization,
-            model.cg_steps,
-        )
-        user_factors_cuda.to_host(user_factors)
-        user_factors[:, :n_user_explicit_factors] = user_explicit_factors
-        user_factors_cuda = CuDenseMatrix(user_factors)
-
-        solver.least_squares(
-            iu_csr_cuda,
-            item_factors_cuda,
-            user_factors_cuda,
-            model.regularization,
-            model.cg_steps,
-        )
-        item_factors_cuda.to_host(item_factors)
-        item_factors[:, n_factors - n_item_explicit_factors :] = item_explicit_factors
-        item_factors_cuda = CuDenseMatrix(item_factors)
-
-    user_factors_cuda.to_host(user_factors)
-    item_factors_cuda.to_host(item_factors)
+        features_model = CpuAlternatingLeastSquares(**feaures_model_params)  
+    features_model.item_factors = y_factors
+    features_model.fit(xy_csr)
+    return features_model.user_factors
