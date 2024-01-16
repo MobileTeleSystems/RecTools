8e952d0f193d84c28e09bfbf092d8444b87b7e15
blondered <nykenott@gmail.com>
2024-01-16 18:42:07 +0300

    added tests and docstrings

--- rectools/visuals/visual_app.py
+++ rectools/visuals/visual_app.py
@@ -1,10 +1,8 @@
-# pylint: disable=too-many-branches
-
 import typing as tp
 
 import ipywidgets as widgets
-import pandas as pd
 import numpy as np
+import pandas as pd
 from IPython.display import display
 
 from rectools import Columns
@@ -12,12 +10,11 @@ from rectools import Columns
 
 class AppDataStorage:
     """
-    Helper class to hold all data for showcase purposes.
-    - Holds info about interactions, recommendations and item_data
-    - Holds `requests_dict`
+    Storage and processing of data for `VisualApp` widgets. This class is not meant to be used
+    directly. Use `VisualApp` class instead
     """
 
-    def __init__(  # noqa: C901
+    def __init__(
         self,
         recos: tp.Dict[tp.Hashable, pd.DataFrame],
         item_data: pd.DataFrame,
@@ -38,12 +35,16 @@ class AppDataStorage:
             raise KeyError(f"Missed {Columns.Item} column in item_data")
         self.item_data = item_data
 
-        if interactions is None:
-            if is_u2i:
-                raise ValueError("For u2i reco showcase you must specify interactions")
+        if interactions is None and is_u2i:
+            raise ValueError("For u2i reco you must specify interactions")
+        if interactions is not None and not is_u2i:
+            raise ValueError("For i2i reco you shouldn't specify interactions")
+        if not is_u2i:
             interactions = self._prepare_interactions_for_i2i()
-        self.interactions = interactions
+        self.interactions: pd.DataFrame = interactions
 
+        if not self.requests_dict:
+            raise ValueError("`requests_dict` is empty")
         self.processed_interactions = self._process_interactions()
         self.processed_recos = self._process_recos()
 
@@ -52,7 +53,7 @@ class AppDataStorage:
         for request_name, request_id in self.requests_dict.items():
             prepared_interactions[request_name] = (
                 self.interactions[self.interactions[self.request_colname] == request_id]
-                .merge(self.item_data, how="left", on=Columns.Item)  # is this correct for i2i?
+                .merge(self.item_data, how="left", on=Columns.Item)
                 .drop(columns=[self.request_colname])
             )
         return prepared_interactions
@@ -94,9 +95,85 @@ class AppDataStorage:
 
 
 class VisualApp:
-    """
-    Main class for recommendations visualization.
-    - Provides visual information about requests in `requests_dict` and recos
+    r"""
+    Main tool for recommendations visualization. Creates Jupyter widgets for visual analysis and
+    comparison of recommendations. Outputs both interactions history and recos for all of the
+    requests. `VisualApp` supports user-to-item (u2i) and item-to-item (i2i) cases.
+
+    Models for comparison will be listed from the `recos` dictionary keys.
+    Users (for u2i case) or items (for i2i case) display names for comparison will be listed from the
+    `requests_dict` keys and ids will be taken for values.
+    For u2i case please provide `interactions` and set `is_u2i` = True. For i2i case keep
+    `interactions` as a default `None` and set `is_u2i` = False.
+
+    Parameters
+    ----------
+    recos : tp.Dict[tp.Hashable, pd.DataFrame]
+        Recommendations from different models in a form of a dict. Model names are supposed to be
+        dict keys. Recommendations from models are supposed to be in form of pandas DataFrames with
+        columns:
+            - `Columns.Item` - recommended item id
+            - `Columns.User` - user id (only for u2i)
+            - `Columns.TargetItem` - target item id (only for i2i)
+            - Any other columns that you wish to display in widgets (e.g. rank or score)
+        The original order of the rows will be preserved. Keep in mind to sort the rows correctly
+        before visualizing. The most intuitive wy is to sort by rank in ascending order.
+    item_data : pd.DataFrame
+        Data for items that is used for visualisation in both interactions and recos widgets.
+        Supposed to be in form of a pandas DataFrame with columns:
+            - `Columns.Item` - item id
+            - Any other columns with item data (e.g. name, category, popularity, image link)
+    requests_dict : tp.Dict[tp.Hashable, tp.Hashable]
+        Predefined requests that will be displayed in widgets. For u2i case specific users are
+        considered as requests. For i2i case - specific items. Their names must be specified as keys
+        of the dict and ids as values of the dict.
+    is_u2i : bool, optional
+        User-to-item recommendation case (opposed to item-to-item), by default True
+    interactions : tp.Optional[pd.DataFrame], optional
+        Table with interactions history for users. Only needed for u2i case. Supposed to be in form
+        of pandas DataFrames with columns:
+            - `Columns.User` - user id
+            - `Columns.Item` - item id
+        The original order of the rows will be preserved. Keep in mind to sort the rows correctly
+        before visualizing. The most intuitive wy is to sort by date in descending order. If user
+        has too many interactions the lest ones may not be displayed.
+        By default None.
+    auto_display : bool, optional
+        Display widgets right after initialization, by default True
+    formatters : tp.Optional[tp.Dict[str, tp.Callable]], optional
+        Formatter functions to apply to columns elements in the sections of interactions and recos.
+        Keys of the dict must be columns names (item_data, interactions and recos columns can be
+        specified here). Values bust be functions that will be applied to corresponding columns
+        elements. The result of each function must be a unicode string that represents html code.
+        Formatters can be used to format text, create links and display images with html.
+        By default None.
+    rows_limit : int, optional
+        Maximum number of rows to display in the sections of interactions and recos, by default 20
+
+    Examples
+    --------
+    >>> recos = {
+    ...     "model1": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+    ...     "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [5, 6], Columns.Rank: [1, 1]})
+    ... }
+    >>>
+    >>> item_data = pd.DataFrame({
+    ...     Columns.Item: [3, 4, 5, 6, 7, 8],
+    ...     "feature_1": ["one", "two", "three", "five", "one", "two"]
+    ... })
+    >>>
+    >>> interactions = pd.DataFrame({Columns.User: [1, 1, 2], Columns.Item: [3, 7, 8]})
+    >>> requests_dict = {"user_one": 1}
+    >>> formatters = {"item_id": lambda x: f"<b>{x}</b>"}
+    >>>
+    >>> widgets = VisualApp(
+    ...     recos=recos,
+    ...     item_data=item_data,
+    ...     interactions=interactions,
+    ...     requests_dict=requests_dict,
+    ...     formatters=formatters,
+    ...     auto_display=False
+    ... )
     """
 
     def __init__(
@@ -107,7 +184,7 @@ class VisualApp:
         is_u2i: bool = True,
         interactions: tp.Optional[pd.DataFrame] = None,
         auto_display: bool = True,
-        item_df_formatters: tp.Optional[tp.Dict[str, tp.Callable]] = None,
+        formatters: tp.Optional[tp.Dict[str, tp.Callable]] = None,
         rows_limit: int = 20,
     ) -> None:
         self.data_storage = AppDataStorage(
@@ -118,7 +195,7 @@ class VisualApp:
             is_u2i=is_u2i,
         )
         self.rows_limit = rows_limit
-        self.item_data_formatters = item_df_formatters if item_df_formatters is not None else {}
+        self.formatters = formatters if formatters is not None else {}
         if auto_display:
             self.display()
 
@@ -127,7 +204,7 @@ class VisualApp:
             df.to_html(
                 escape=False,
                 index=False,
-                formatters=self.item_data_formatters,
+                formatters=self.formatters,
                 max_rows=self.rows_limit,
                 border=0,
             )
--- tests/visuals/test_visual_app.py
+++ tests/visuals/test_visual_app.py
@@ -4,40 +4,38 @@ import pandas as pd
 import pytest
 
 from rectools import Columns
-from rectools.visuals.visual_app import AppDataStorage
+from rectools.visuals.visual_app import AppDataStorage, VisualApp
+
+RECOS_U2I: tp.Dict[tp.Hashable, pd.DataFrame] = {
+    "model1": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+    "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
+}
+
+RECOS_I2I: tp.Dict[tp.Hashable, pd.DataFrame] = {
+    "model1": pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+    "model2": pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
+}
+
+ITEM_DATA = pd.DataFrame({Columns.Item: [3, 4, 5, 6, 7, 8], "feature_1": ["one", "two", "three", "five", "one", "two"]})
+INTERACTIONS = pd.DataFrame({Columns.User: [1, 1, 2], Columns.Item: [3, 7, 8]})
+REQUESTS_DICT_U2I: tp.Dict[tp.Hashable, tp.Hashable] = {"user_one": 1}
+REUQESTS_DICT_I2I: tp.Dict[tp.Hashable, tp.Hashable] = {"item_three": 3}
 
 
 class TestAppDataStorage:
-    @pytest.fixture
-    def recos(self) -> tp.Dict[tp.Hashable, pd.DataFrame]:
-        recos = {
-            "model1": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
-            "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
-        }
-        return recos
+    def test_u2i(self) -> None:
 
-    @pytest.fixture
-    def item_data(self) -> pd.DataFrame:
-        item_data = pd.DataFrame(
-            {Columns.Item: [3, 4, 5, 6, 7, 8], "feature_1": ["one", "two", "three", "five", "one", "two"]}
-        )
-        return item_data
-
-    @pytest.fixture
-    def interactions(self) -> pd.DataFrame:
-        interactions = pd.DataFrame({Columns.User: [1, 1, 2], Columns.Item: [3, 7, 8]})
-        return interactions
-
-    def test_u2i(self, recos: tp.Dict[tp.Hashable, pd.DataFrame], item_data: pd.DataFrame, interactions: pd.DataFrame):
-        # corner case: empty dict, no available ids
-        requests_dict = {"user_one": 1}
         ads = AppDataStorage(
-            recos=recos, item_data=item_data, interactions=interactions, is_u2i=True, requests_dict=requests_dict
+            recos=RECOS_U2I,
+            item_data=ITEM_DATA,
+            interactions=INTERACTIONS,
+            is_u2i=True,
+            requests_dict=REQUESTS_DICT_U2I,
         )
 
         assert ads.is_u2i
         assert ads.request_colname == Columns.User
-        assert ads.requests_dict == requests_dict
+        assert ads.requests_dict == REQUESTS_DICT_U2I
         assert ads.model_names == ["model1", "model2"]
         assert ads.request_names == ["user_one"]
 
@@ -55,4 +53,129 @@ class TestAppDataStorage:
             for user_name, user_recos in model_recos.items():
                 pd.testing.assert_frame_equal(user_recos, ads.processed_recos[model_name][user_name])
 
+    def test_i2i(self) -> None:
+        ads = AppDataStorage(recos=RECOS_I2I, item_data=ITEM_DATA, is_u2i=False, requests_dict=REUQESTS_DICT_I2I)
 
+        assert not ads.is_u2i
+        assert ads.request_colname == Columns.TargetItem
+        assert ads.requests_dict == REUQESTS_DICT_I2I
+        assert ads.model_names == ["model1", "model2"]
+        assert ads.request_names == ["item_three"]
+
+        expected_i2i_interactions = pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [3, 4]})
+        pd.testing.assert_frame_equal(expected_i2i_interactions, ads.interactions, check_like=True)
+
+        assert list(ads.processed_interactions.keys()) == ["item_three"]
+        expected_interactions = pd.DataFrame({Columns.Item: [3], "feature_1": ["one"]})
+        pd.testing.assert_frame_equal(ads.processed_interactions["item_three"], expected_interactions)
+
+        expected_processed_recos = {
+            "model1": {"item_three": pd.DataFrame({Columns.Item: [3], "feature_1": ["one"], Columns.Score: [0.99]})},
+            "model2": {"item_three": pd.DataFrame({Columns.Item: [5], "feature_1": ["three"], Columns.Rank: [1]})},
+        }
+        assert expected_processed_recos.keys() == ads.processed_recos.keys()
+        for model_name, model_recos in expected_processed_recos.items():
+            assert model_recos.keys() == ads.processed_recos[model_name].keys()
+            for user_name, user_recos in model_recos.items():
+                pd.testing.assert_frame_equal(user_recos, ads.processed_recos[model_name][user_name])
+
+    def test_missing_columns_validation(self) -> None:
+
+        # Missing `Columns.User` for u2i
+        with pytest.raises(KeyError):
+            incorrect_u2i_recos: tp.Dict[tp.Hashable, pd.DataFrame] = {
+                "model1": pd.DataFrame({Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+                "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
+            }
+            AppDataStorage(
+                recos=incorrect_u2i_recos,
+                item_data=ITEM_DATA,
+                is_u2i=True,
+                requests_dict=REQUESTS_DICT_U2I,
+                interactions=INTERACTIONS,
+            )
+
+        # Missing `Columns.Item`
+        with pytest.raises(KeyError):
+            incorrect_u2i_recos = {
+                "model1": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+                "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Rank: [1, 1]}),
+            }
+            AppDataStorage(
+                recos=incorrect_u2i_recos,
+                item_data=ITEM_DATA,
+                is_u2i=True,
+                requests_dict=REQUESTS_DICT_U2I,
+                interactions=INTERACTIONS,
+            )
+
+        # Missing `Columns.TargetItem` for i2i
+        with pytest.raises(KeyError):
+            AppDataStorage(recos=RECOS_U2I, item_data=ITEM_DATA, is_u2i=False, requests_dict=REUQESTS_DICT_I2I)
+
+        # Missing `Columns.Item` in item_data
+        with pytest.raises(KeyError):
+            AppDataStorage(
+                recos=RECOS_U2I,
+                item_data=ITEM_DATA.drop(columns=[Columns.Item]),
+                interactions=INTERACTIONS,
+                is_u2i=True,
+                requests_dict=REQUESTS_DICT_U2I,
+            )
+
+    def test_incorrect_interactions_for_reco_case(self) -> None:
+
+        # u2i without interactions
+        with pytest.raises(ValueError):
+            AppDataStorage(recos=RECOS_U2I, item_data=ITEM_DATA, is_u2i=True, requests_dict=REQUESTS_DICT_U2I)
+
+        # i2i with interactions
+        with pytest.raises(ValueError):
+            AppDataStorage(
+                recos=RECOS_I2I,
+                item_data=ITEM_DATA,
+                is_u2i=False,
+                requests_dict=REUQESTS_DICT_I2I,
+                interactions=INTERACTIONS,
+            )
+
+    def test_empty_requests(self) -> None:
+        with pytest.raises(ValueError):
+            AppDataStorage(
+                recos=RECOS_U2I,
+                item_data=ITEM_DATA,
+                interactions=INTERACTIONS,
+                is_u2i=True,
+                requests_dict={},
+            )
+
+
+class TestVisualApp:
+    @pytest.mark.parametrize("auto_display", (True, False))
+    @pytest.mark.parametrize("formatters", (None, {"feature_1": lambda x: f"<b>{x}</b>"}))
+    def test_happy_path_u2i(
+        self, auto_display: bool, formatters: tp.Optional[tp.Dict[str, tp.Callable]]
+    ) -> None:
+        VisualApp(
+            recos=RECOS_U2I,
+            item_data=ITEM_DATA,
+            requests_dict=REQUESTS_DICT_U2I,
+            is_u2i=True,
+            interactions=INTERACTIONS,
+            auto_display=auto_display,
+            formatters=formatters,
+        )
+
+    @pytest.mark.parametrize("auto_display", (True, False))
+    @pytest.mark.parametrize("formatters", (None, {"feature_1": lambda x: f"<b>{x}</b>"}))
+    def test_happy_path_i2i(
+        self, auto_display: bool, formatters: tp.Optional[tp.Dict[str, tp.Callable]]
+    ) -> None:
+        VisualApp(
+            recos=RECOS_I2I,
+            item_data=ITEM_DATA,
+            requests_dict=REUQESTS_DICT_I2I,
+            is_u2i=False,
+            auto_display=auto_display,
+            formatters=formatters,
+        )
