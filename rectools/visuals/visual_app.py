ee8dbb20cd7d34b91ce951d3275fd732e520840a
Daria <93913290+blondered@users.noreply.github.com>
2024-01-31 16:57:49 +0300

    Feature/extend visuals (#82)
    - added random requests to VisualApp and ItemToItemVisualApp
    - added parameter to control min column width

--- rectools/visuals/visual_app.py
+++ rectools/visuals/visual_app.py
@@ -1,12 +1,16 @@
 import typing as tp
 
 import ipywidgets as widgets
+import numpy as np
 import pandas as pd
 from IPython.display import display
 
 from rectools import Columns
+from rectools.utils import fast_isin
 
 TablesDict = tp.Dict[tp.Hashable, pd.DataFrame]
+
+MIN_WIDTH_LIMIT = 10
 
 
 class _AppDataStorage:
@@ -21,13 +25,21 @@ class _AppDataStorage:
         item_data: pd.DataFrame,
         selected_requests: tp.Dict[tp.Hashable, tp.Hashable],
         is_u2i: bool = True,
+        n_random_requests: int = 0,
         interactions: tp.Optional[pd.DataFrame] = None,
     ) -> None:
-        self.request_colname = Columns.User if is_u2i else Columns.TargetItem
+        self.id_col = Columns.User if is_u2i else Columns.TargetItem
         self.is_u2i = is_u2i
-        self.selected_requests = selected_requests
-        self.model_names = list(recos.keys())
-        self.request_names = list(selected_requests.keys())
+
+        if n_random_requests > 0:
+            self.selected_requests = self._fill_requests_with_random(
+                selected_requests=selected_requests,
+                n_random_requests=n_random_requests,
+                id_col=self.id_col,
+                recos=recos,
+            )
+        else:
+            self.selected_requests = selected_requests
 
         self._check_columns_present_in_recos(recos)
         self.recos = recos
@@ -49,32 +61,64 @@ class _AppDataStorage:
         self.grouped_interactions = self._group_interactions(
             interactions=self.interactions,
             selected_requests=self.selected_requests,
-            request_colname=self.request_colname,
+            id_col=self.id_col,
             item_data=self.item_data,
         )
         self.grouped_recos = self._group_recos(
             recos=self.recos,
             selected_requests=self.selected_requests,
-            request_colname=self.request_colname,
+            id_col=self.id_col,
             item_data=self.item_data,
         )
+
+    @property
+    def request_names(self) -> tp.List[tp.Hashable]:
+        return list(self.selected_requests.keys())
+
+    @property
+    def model_names(self) -> tp.List[tp.Hashable]:
+        return list(self.grouped_recos.keys())
+
+    @classmethod
+    def _fill_requests_with_random(
+        cls,
+        selected_requests: tp.Dict[tp.Hashable, tp.Hashable],
+        n_random_requests: int,
+        id_col: str,
+        recos: TablesDict,
+    ) -> tp.Dict[tp.Hashable, tp.Hashable]:
+
+        # Leave only those ids that were not predefined by user
+        # Request ids (e.g. user ids) are stored as values in `selected_requests`
+        all_ids = [model_recos[id_col].unique() for model_recos in recos.values()]
+        unique_ids = pd.unique(np.hstack(all_ids))
+        selected_ids = np.array(list(selected_requests.values()))
+        selected_mask = fast_isin(unique_ids, selected_ids)
+        selecting_from = unique_ids[~selected_mask]
+
+        num_selecting = min(len(selecting_from), n_random_requests)
+        new_ids = np.random.choice(selecting_from, num_selecting, replace=False)
+        res = selected_requests.copy()
+        new_requests: tp.Dict[tp.Hashable, tp.Hashable] = {f"random_{i+1}": new_id for i, new_id in enumerate(new_ids)}
+        res.update(new_requests)
+        return res
 
     @classmethod
     def _group_interactions(
         cls,
         interactions: pd.DataFrame,
         selected_requests: tp.Dict[tp.Hashable, tp.Hashable],
-        request_colname: str,
+        id_col: str,
         item_data: pd.DataFrame,
     ) -> tp.Dict[tp.Hashable, pd.DataFrame]:
-        # request ids (e.g. user ids) are stored as values in `selected_requests`
-        selected_interactions = interactions[interactions[request_colname].isin(selected_requests.values())]
+        # Request ids (e.g. user ids) are stored as values in `selected_requests`
+        selected_interactions = interactions[interactions[id_col].isin(selected_requests.values())]
         selected_interactions = selected_interactions.merge(item_data, how="left", on="item_id")
         prepared_interactions = {}
         for request_name, request_id in selected_requests.items():
             prepared_interactions[request_name] = selected_interactions[
-                selected_interactions[request_colname] == request_id
-            ].drop(columns=[request_colname])
+                selected_interactions[id_col] == request_id
+            ].drop(columns=[id_col])
         return prepared_interactions
 
     @classmethod
@@ -82,26 +126,26 @@ class _AppDataStorage:
         cls,
         recos: TablesDict,
         selected_requests: tp.Dict[tp.Hashable, tp.Hashable],
-        request_colname: str,
+        id_col: str,
         item_data: pd.DataFrame,
     ) -> tp.Dict[tp.Hashable, TablesDict]:
         prepared_recos = {}
         for model_name, model_recos in recos.items():
-            # request ids (e.g. user ids) are stored as values in `selected_requests`
-            selected_recos = model_recos[model_recos[request_colname].isin(selected_requests.values())]
+            # Request ids (e.g. user ids) are stored as values in `selected_requests`
+            selected_recos = model_recos[model_recos[id_col].isin(selected_requests.values())]
             prepared_model_recos = {}
             for request_name, request_id in selected_requests.items():
                 prepared_model_recos[request_name] = item_data.merge(
-                    selected_recos[selected_recos[request_colname] == request_id],
+                    selected_recos[selected_recos[id_col] == request_id],
                     how="right",
                     on="item_id",
                     suffixes=["_item", "_recos"],
-                ).drop(columns=[request_colname])
+                ).drop(columns=[id_col])
             prepared_recos[model_name] = prepared_model_recos
         return prepared_recos
 
     def _check_columns_present_in_recos(self, recos: TablesDict) -> None:
-        required_columns = {Columns.User, Columns.Item} if self.is_u2i else {Columns.TargetItem, Columns.Item}
+        required_columns = {self.id_col, Columns.Item}
         for model_name, model_recos in recos.items():
             actual_columns = set(model_recos.columns)
             if not actual_columns >= required_columns:
@@ -128,11 +172,15 @@ class VisualAppBase:
         auto_display: bool = True,
         formatters: tp.Optional[tp.Dict[str, tp.Callable]] = None,
         rows_limit: int = 20,
+        min_width: int = 100,
         **kwargs: tp.Any,
     ) -> None:
         self.rows_limit = rows_limit
         self.formatters = formatters if formatters is not None else {}
         self.data_storage: _AppDataStorage = self._create_data_storage(*args, **kwargs)
+        if min_width <= MIN_WIDTH_LIMIT:
+            raise ValueError(f"`min_width` must be greater then {MIN_WIDTH_LIMIT}. {min_width} specified")
+        self.min_width = min_width
         if auto_display:
             self.display()
 
@@ -149,7 +197,7 @@ class VisualAppBase:
                 border=0,
             )
             .replace("<td>", '<td align="center">')
-            .replace("<th>", '<th style="text-align: center; min-width: 100px;">')
+            .replace("<th>", f'<th style="text-align: center; min-width: {self.min_width}px;">')
         )
         return html_repr
 
@@ -172,7 +220,7 @@ class VisualAppBase:
     def _display_request_id(self, request_name: str) -> None:
         """Display request_id for `request_name`"""
         request_id = self.data_storage.selected_requests[request_name]
-        display(widgets.HTML(value=f"{self.data_storage.request_colname}: {request_id}"))
+        display(widgets.HTML(value=f"{self.data_storage.id_col}: {request_id}"))
 
     def _display_model_name(self, model_name: str) -> None:
         """Display model_name"""
@@ -182,7 +230,7 @@ class VisualAppBase:
         """Display full VisualApp widget"""
         request_name_selection = widgets.ToggleButtons(
             options=self.data_storage.request_names,
-            description=f"Request {self.data_storage.request_colname}:",
+            description=f"Request {self.data_storage.id_col}:",
             disabled=False,
             button_style="warning",
         )
@@ -249,6 +297,14 @@ class VisualApp(VisualAppBase):
             - Any other columns that you wish to display in widgets (e.g. rank or score)
         The original order of the rows will be preserved. Keep in mind to sort the rows correctly
         before visualizing. The most intuitive way is to sort by rank in ascending order.
+    interactions : pd.DataFrame
+        Table with interactions history for users. Only needed for u2i case. Supposed to be in form
+        of pandas DataFrames with columns:
+            - `Columns.User` - user id
+            - `Columns.Item` - item id
+        The original order of the rows will be preserved. Keep in mind to sort the rows correctly
+        before visualizing. The most intuitive way is to sort by date in descending order. If user
+        has too many interactions the lest ones may not be displayed.
     item_data : pd.DataFrame
         Data for items that is used for visualisation in both interactions and recos widgets.
         Supposed to be in form of a pandas DataFrame with columns:
@@ -257,14 +313,8 @@ class VisualApp(VisualAppBase):
     selected_users : tp.Dict[tp.Hashable, tp.Hashable]
         Predefined users that will be displayed in widgets. User names must be specified as keys
         of the dict and user ids as values of the dict.
-    interactions : tp.Optional[pd.DataFrame], optional, default ``None``
-        Table with interactions history for users. Only needed for u2i case. Supposed to be in form
-        of pandas DataFrames with columns:
-            - `Columns.User` - user id
-            - `Columns.Item` - item id
-        The original order of the rows will be preserved. Keep in mind to sort the rows correctly
-        before visualizing. The most intuitive way is to sort by date in descending order. If user
-        has too many interactions the lest ones may not be displayed.
+    n_random_users : int, default 0
+        Number of random users to add for visualization from users in recommendation tables.
     auto_display : bool, optional, default ``True``
         Display widgets right after initialization.
     formatters : tp.Optional[tp.Dict[str, tp.Callable]], optional, default ``None``
@@ -275,6 +325,9 @@ class VisualApp(VisualAppBase):
         Formatters can be used to format text, create links and display images with html.
     rows_limit : int, optional, default 20
         Maximum number of rows to display in the sections of interactions and recos.
+    min_width : int, optional, default 100
+        Minimum column width in pixels for dataframe columns in widgets output. Must be greater then
+        10.
 
     Examples
     --------
@@ -308,18 +361,22 @@ class VisualApp(VisualAppBase):
         interactions: pd.DataFrame,
         item_data: pd.DataFrame,
         selected_users: tp.Dict[tp.Hashable, tp.Hashable],
+        n_random_users: int = 0,
         auto_display: bool = True,
         formatters: tp.Optional[tp.Dict[str, tp.Callable]] = None,
         rows_limit: int = 20,
+        min_width: int = 100,
     ) -> None:
         super().__init__(
             recos=recos,
             interactions=interactions,
             item_data=item_data,
             selected_users=selected_users,
+            n_random_users=n_random_users,
             auto_display=auto_display,
             formatters=formatters,
             rows_limit=rows_limit,
+            min_width=min_width,
         )
 
     def _create_data_storage(
@@ -328,6 +385,7 @@ class VisualApp(VisualAppBase):
         interactions: pd.DataFrame,
         item_data: pd.DataFrame,
         selected_users: tp.Dict[tp.Hashable, tp.Hashable],
+        n_random_users: int,
     ) -> _AppDataStorage:
         return _AppDataStorage(
             interactions=interactions,
@@ -335,6 +393,7 @@ class VisualApp(VisualAppBase):
             selected_requests=selected_users,
             item_data=item_data,
             is_u2i=True,
+            n_random_requests=n_random_users,
         )
 
 
@@ -369,6 +428,8 @@ class ItemToItemVisualApp(VisualAppBase):
     selected_items : tp.Dict[tp.Hashable, tp.Hashable]
         Predefined items that will be displayed in widgets. Item names must be specified as keys
         of the dict and item ids as values of the dict.
+    n_random_items : int, default 0
+        Number of random items to add for visualization from target items in recommendation tables.
     auto_display : bool, optional, default ``True``
         Display widgets right after initialization.
     formatters : tp.Optional[tp.Dict[str, tp.Callable]], optional, default ``None``
@@ -379,6 +440,9 @@ class ItemToItemVisualApp(VisualAppBase):
         Formatters can be used to format text, create links and display images with html.
     rows_limit : int, optional, default 20
         Maximum number of rows to display in the sections of interactions and recos.
+    min_width : int, optional, default 100
+        Minimum column width in pixels for dataframe columns in widgets output. Must be greater then
+        10.
 
     Examples
     --------
@@ -409,17 +473,21 @@ class ItemToItemVisualApp(VisualAppBase):
         recos: TablesDict,
         item_data: pd.DataFrame,
         selected_items: tp.Dict[tp.Hashable, tp.Hashable],
+        n_random_items: int = 0,
         auto_display: bool = True,
         formatters: tp.Optional[tp.Dict[str, tp.Callable]] = None,
         rows_limit: int = 20,
+        min_width: int = 100,
     ) -> None:
         super().__init__(
             recos=recos,
             item_data=item_data,
             selected_items=selected_items,
+            n_random_items=n_random_items,
             auto_display=auto_display,
             formatters=formatters,
             rows_limit=rows_limit,
+            min_width=min_width,
         )
 
     def _create_data_storage(
@@ -427,10 +495,12 @@ class ItemToItemVisualApp(VisualAppBase):
         recos: TablesDict,
         item_data: pd.DataFrame,
         selected_items: tp.Dict[tp.Hashable, tp.Hashable],
+        n_random_items: int,
     ) -> _AppDataStorage:
         return _AppDataStorage(
             recos=recos,
             selected_requests=selected_items,
             item_data=item_data,
             is_u2i=False,
+            n_random_requests=n_random_items,
         )
--- tests/visuals/test_visual_app.py
+++ tests/visuals/test_visual_app.py
@@ -1,5 +1,6 @@
 import typing as tp
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -7,12 +8,16 @@ from rectools import Columns
 from rectools.visuals.visual_app import ItemToItemVisualApp, TablesDict, VisualApp, _AppDataStorage
 
 RECOS_U2I: TablesDict = {
-    "model1": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
-    "model2": pd.DataFrame({Columns.User: [1, 2], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
+    "model1": pd.DataFrame(
+        {Columns.User: [1, 2, 3, 4], Columns.Item: [3, 4, 3, 4], Columns.Score: [0.99, 0.9, 0.5, 0.5]}
+    ),
+    "model2": pd.DataFrame({Columns.User: [1, 2, 3, 4], Columns.Item: [5, 6, 5, 6], Columns.Rank: [1, 1, 1, 1]}),
 }
 
 RECOS_I2I: TablesDict = {
-    "model1": pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [3, 4], Columns.Score: [0.99, 0.9]}),
+    "model1": pd.DataFrame(
+        {Columns.TargetItem: [3, 4, 5, 5], Columns.Item: [3, 4, 7, 8], Columns.Score: [0.99, 0.9, 0.7, 0.5]}
+    ),
     "model2": pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [5, 6], Columns.Rank: [1, 1]}),
 }
 
@@ -34,7 +39,7 @@ class TestAppDataStorage:
         )
 
         assert ads.is_u2i
-        assert ads.request_colname == Columns.User
+        assert ads.id_col == Columns.User
         assert ads.selected_requests == SELECTED_REQUESTS_U2I
         assert ads.model_names == ["model1", "model2"]
         assert ads.request_names == ["user_one"]
@@ -59,12 +64,12 @@ class TestAppDataStorage:
         )
 
         assert not ads.is_u2i
-        assert ads.request_colname == Columns.TargetItem
+        assert ads.id_col == Columns.TargetItem
         assert ads.selected_requests == SELECTED_REQUESTS_I2I
         assert ads.model_names == ["model1", "model2"]
         assert ads.request_names == ["item_three"]
 
-        expected_i2i_interactions = pd.DataFrame({Columns.TargetItem: [3, 4], Columns.Item: [3, 4]})
+        expected_i2i_interactions = pd.DataFrame({Columns.TargetItem: [3, 4, 5], Columns.Item: [3, 4, 5]})
         pd.testing.assert_frame_equal(expected_i2i_interactions, ads.interactions, check_like=True)
 
         assert list(ads.grouped_interactions.keys()) == ["item_three"]
@@ -151,11 +156,85 @@ class TestAppDataStorage:
                 selected_requests={},
             )
 
+    @pytest.mark.parametrize("n_random_requests", (1, 5))
+    def test_u2i_with_random_requests(self, n_random_requests: int) -> None:
+        ads = _AppDataStorage(
+            recos=RECOS_U2I,
+            item_data=ITEM_DATA,
+            interactions=INTERACTIONS,
+            is_u2i=True,
+            selected_requests=SELECTED_REQUESTS_U2I,
+            n_random_requests=n_random_requests,
+        )
+        assert "user_one" in ads.request_names
+        corrected_n_random_requests = min(n_random_requests, 3)  # only 3 users in recos can be selected from
+
+        for i in range(1, corrected_n_random_requests + 1):
+            random_name = f"random_{i}"
+            random_id = ads.selected_requests[random_name]
+            assert random_name in ads.request_names
+            assert random_id != 1  # random id is not same as predefined by user
+            total_recos = 0
+            for model_name in ["model1", "model2"]:
+                expected_recos = RECOS_U2I[model_name].query(f"{Columns.User} == @random_id")["item_id"].sort_values()
+                actual_recos = ads.grouped_recos[model_name][random_name]["item_id"].sort_values()
+                total_recos += expected_recos.shape[0]
+                assert np.array_equal(actual_recos, expected_recos)
+            assert total_recos > 0  # random user has recos at least from one model
+
+        # correct names in selected_requests
+        all_selected_names = set(ads.selected_requests.keys())
+        assert all_selected_names == set(
+            ["user_one"] + [f"random_{i}" for i in range(1, corrected_n_random_requests + 1)]
+        )
+
+        # random ids don't have duplicates
+        assert len(ads.selected_requests.values()) == len(set(ads.selected_requests.values()))
+
+    @pytest.mark.parametrize("n_random_requests", (2, 5))
+    def test_i2i_with_random_requests(self, n_random_requests: int) -> None:
+        ads = _AppDataStorage(
+            recos=RECOS_I2I,
+            item_data=ITEM_DATA,
+            is_u2i=False,
+            selected_requests=SELECTED_REQUESTS_I2I,
+            n_random_requests=n_random_requests,
+        )
+        assert "item_three" in ads.request_names
+        corrected_n_random_requests = min(n_random_requests, 2)  # only 2 target items in recos can be selected from
+
+        for i in range(1, corrected_n_random_requests + 1):
+            random_name = f"random_{i}"
+            random_id = ads.selected_requests[random_name]
+            assert random_name in ads.request_names
+            assert random_id != 3  # random id is not same as predefined by user
+            total_recos = 0
+            for model_name in ["model1", "model2"]:
+                expected_recos = (
+                    RECOS_I2I[model_name].query(f"{Columns.TargetItem} == @random_id")["item_id"].sort_values()
+                )
+                actual_recos = ads.grouped_recos[model_name][random_name]["item_id"].sort_values()
+                total_recos += expected_recos.shape[0]
+                assert np.array_equal(actual_recos, expected_recos)
+            assert total_recos > 0  # random item has recos at least from one model
+
+        # correct names in selected_requests
+        all_selected_names = set(ads.selected_requests.keys())
+        assert all_selected_names == set(
+            ["item_three"] + [f"random_{i}" for i in range(1, corrected_n_random_requests + 1)]
+        )
+
+        # random ids don't have duplicates
+        assert len(ads.selected_requests.values()) == len(set(ads.selected_requests.values()))
+
 
 class TestVisualApp:
     @pytest.mark.parametrize("auto_display", (True, False))
+    @pytest.mark.parametrize("n_random_users", (0, 2, 100))
     @pytest.mark.parametrize("formatters", (None, {"feature_1": lambda x: f"<b>{x}</b>"}))
-    def test_happy_path(self, auto_display: bool, formatters: tp.Optional[tp.Dict[str, tp.Callable]]) -> None:
+    def test_happy_path(
+        self, auto_display: bool, n_random_users: int, formatters: tp.Optional[tp.Dict[str, tp.Callable]]
+    ) -> None:
         VisualApp(
             recos=RECOS_U2I,
             item_data=ITEM_DATA,
@@ -163,17 +242,45 @@ class TestVisualApp:
             interactions=INTERACTIONS,
             auto_display=auto_display,
             formatters=formatters,
+            n_random_users=n_random_users,
         )
+
+    def test_incorrect_min_width(self) -> None:
+        with pytest.raises(ValueError):
+            VisualApp(
+                recos=RECOS_U2I,
+                item_data=ITEM_DATA,
+                selected_users=SELECTED_REQUESTS_U2I,
+                interactions=INTERACTIONS,
+                auto_display=True,
+                n_random_users=0,
+                min_width=5,
+            )
 
 
 class TestItemToItemVisualApp:
     @pytest.mark.parametrize("auto_display", (True, False))
+    @pytest.mark.parametrize("n_random_items", (0, 2, 100))
     @pytest.mark.parametrize("formatters", (None, {"feature_1": lambda x: f"<b>{x}</b>"}))
-    def test_happy_path(self, auto_display: bool, formatters: tp.Optional[tp.Dict[str, tp.Callable]]) -> None:
+    def test_happy_path(
+        self, auto_display: bool, n_random_items: int, formatters: tp.Optional[tp.Dict[str, tp.Callable]]
+    ) -> None:
         ItemToItemVisualApp(
             recos=RECOS_I2I,
             item_data=ITEM_DATA,
             selected_items=SELECTED_REQUESTS_I2I,
             auto_display=auto_display,
             formatters=formatters,
+            n_random_items=n_random_items,
         )
+
+    def test_incorrect_min_width(self) -> None:
+        with pytest.raises(ValueError):
+            ItemToItemVisualApp(
+                recos=RECOS_I2I,
+                item_data=ITEM_DATA,
+                selected_items=SELECTED_REQUESTS_I2I,
+                auto_display=True,
+                n_random_items=0,
+                min_width=-10,
+            )
