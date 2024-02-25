23a62973f2727d0e067286e3af8df476ac399ea8
Emiliy Feldman <feldlime@yandex.ru>
2024-02-25 11:18:30 +0100

    fixed model tests

--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -48,30 +48,6 @@ def test_raise_when_recommend_i2i_from_not_fitted() -> None:
             k=5,
         )
 
-
-def test_raise_when_recommend_for_nonexistent_user() -> None:
-    model = ModelBase()
-    model.is_fitted = True
-    with pytest.raises(KeyError):
-        model.recommend(
-            users=np.array([10, 90]),
-            dataset=DATASET,
-            k=5,
-            filter_viewed=False,
-        )
-
-
-def test_raise_when_recommend_for_nonexistent_item() -> None:
-    model = ModelBase()
-    model.is_fitted = True
-    with pytest.raises(KeyError):
-        model.recommend_to_items(
-            target_items=np.array([11, 19]),
-            dataset=DATASET,
-            k=5,
-        )
-
-
 @pytest.mark.parametrize("k", (-4, 0))
 def test_raise_when_k_is_not_positive_u2i(k: int) -> None:
     model = ModelBase()
@@ -97,157 +73,127 @@ def test_raise_when_k_is_not_positive_i2i(k: int) -> None:
         )
 
 
-class SomeModel(ModelBase):
-    def _fit(self, dataset: Dataset, *args: tp.Any, **kwargs: tp.Any) -> None:
-        pass
+class TestRecommendWithInternalIds:
+    def setup(self) -> None:
+        class SomeModel(ModelBase):
+            def _fit(self, dataset: Dataset, *args: tp.Any, **kwargs: tp.Any) -> None:
+                pass
 
-    def _recommend_u2i(
-        self,
-        user_ids: np.ndarray,
-        dataset: Dataset,
-        k: int,
-        filter_viewed: bool,
-        sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
-    ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
-        return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
+            def _recommend_u2i(
+                self,
+                user_ids: np.ndarray,
+                dataset: Dataset,
+                k: int,
+                filter_viewed: bool,
+                sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
+            ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
+                return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
 
-    def _recommend_i2i(
-        self,
-        target_ids: np.ndarray,
-        dataset: Dataset,
-        k: int,
-        sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
-    ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
-        return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
+            def _recommend_i2i(
+                self,
+                target_ids: np.ndarray,
+                dataset: Dataset,
+                k: int,
+                sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
+            ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
+                return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
+    
+        self.model = SomeModel().fit(DATASET)
 
+    def test_u2i_success(self, mocker: MockerFixture) -> None:
+        model = self.model
+        users = [0, 1]
+        items_to_recommend = [0, 1, 2]
 
-def test_recommend_from_internal_ids(mocker: MockerFixture) -> None:
-    model = SomeModel().fit(DATASET)
-    users = [0, 1, 2]
-    items_to_recommend = [0, 1, 2]
-
-    spy = mocker.spy(model, "_recommend_u2i")
-    model.recommend(
-        users=users,
-        dataset=DATASET,
-        k=2,
-        filter_viewed=False,
-        items_to_recommend=items_to_recommend,
-        assume_external_ids=False,
-    )
-
-    args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
-    assert list(args[0]) == users
-    assert list(args[4]) == items_to_recommend
-
-
-@pytest.mark.parametrize(
-    "users, items_to_recommend",
-    (
-        (["u1", "u2"], [0, 1]),
-        ([0, 1], ["i1", "i2"]),
-        (["u1", "u2"], ["i1", "i2"]),
-    ),
-)
-def test_recommend_from_internal_ids_fails_when_not_integer_ids(users: AnyIds, items_to_recommend: AnyIds) -> None:
-    model = SomeModel().fit(DATASET)
-
-    with pytest.raises(TypeError):
-        model.recommend(
+        spy = mocker.spy(model, "_recommend_u2i")
+        reco = model.recommend(
             users=users,
             dataset=DATASET,
             k=2,
             filter_viewed=False,
             items_to_recommend=items_to_recommend,
             assume_external_ids=False,
+            add_rank_col=False,
         )
 
+        args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
+        assert list(args[0]) == users
+        assert list(args[4]) == items_to_recommend
 
-def test_recommend_returns_internal_ids() -> None:
-    model = SomeModel().fit(DATASET)
+        excepted = pd.DataFrame(
+            {
+                Columns.User: [0, 0, 1],
+                Columns.Item: [0, 1, 2],
+                Columns.Score: [0.1, 0.2, 0.3],
+            }
+        )
+        pd.testing.assert_frame_equal(reco, excepted.astype({Columns.Score: np.float32}))
 
-    reco = model.recommend(
-        users=[10, 20],
-        dataset=DATASET,
-        k=2,
-        filter_viewed=False,
-        items_to_recommend=[11, 12],
-        add_rank_col=False,
-        return_external_ids=False,
+    @pytest.mark.parametrize(
+        "users, items_to_recommend",
+        (
+            (["u1", "u2"], [0, 1]),
+            ([0, 1], ["i1", "i2"]),
+            (["u1", "u2"], ["i1", "i2"]),
+        ),
     )
+    def test_u2i_when_not_integer_ids(self, users: AnyIds, items_to_recommend: AnyIds) -> None:
+        with pytest.raises(TypeError):
+            self.model.recommend(
+                users=users,
+                dataset=DATASET,
+                k=2,
+                filter_viewed=False,
+                items_to_recommend=items_to_recommend,
+                assume_external_ids=False,
+            )
 
-    excepted = pd.DataFrame(
-        {
-            Columns.User: [0, 0, 1],
-            Columns.Item: [0, 1, 2],
-            Columns.Score: [0.1, 0.2, 0.3],
-        }
-    )
+    def test_i2i_success(self, mocker: MockerFixture) -> None:
+        model = self.model
+        target_items = [0, 1, 2]
+        items_to_recommend = [0, 1, 2]
 
-    pd.testing.assert_frame_equal(reco, excepted)
-
-
-def test_recommend_to_items_from_internal_ids(mocker: MockerFixture) -> None:
-    model = SomeModel().fit(DATASET)
-    target_items = [0, 1, 2]
-    items_to_recommend = [0, 1, 2]
-
-    spy = mocker.spy(model, "_recommend_i2i")
-    model.recommend_to_items(
-        target_items=target_items,
-        dataset=DATASET,
-        k=2,
-        items_to_recommend=items_to_recommend,
-        assume_external_ids=False,
-    )
-
-    args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
-    assert list(args[0]) == target_items
-    assert list(args[3]) == items_to_recommend
-
-
-@pytest.mark.parametrize(
-    "target_items, items_to_recommend",
-    (
-        (["i1", "i2"], [0, 1]),
-        ([0, 1], ["i1", "i2"]),
-        (["i1", "i2"], ["i1", "i2"]),
-    ),
-)
-def test_recommend_to_items_from_internal_ids_fails_when_not_integer_ids(
-    target_items: AnyIds, items_to_recommend: AnyIds
-) -> None:
-    model = SomeModel().fit(DATASET)
-
-    with pytest.raises(TypeError):
-        model.recommend_to_items(
+        spy = mocker.spy(model, "_recommend_i2i")
+        reco = model.recommend_to_items(
             target_items=target_items,
             dataset=DATASET,
             k=2,
             items_to_recommend=items_to_recommend,
             assume_external_ids=False,
+            add_rank_col=False,
+            filter_itself=False,
         )
 
+        args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
+        assert list(args[0]) == target_items
+        assert list(args[3]) == items_to_recommend
 
-def test_recommend_to_items_returns_internal_ids() -> None:
-    model = SomeModel().fit(DATASET)
+        excepted = pd.DataFrame(
+            {
+                Columns.TargetItem: [0, 0, 1],
+                Columns.Item: [0, 1, 2],
+                Columns.Score: [0.1, 0.2, 0.3],
+            }
+        )
+        pd.testing.assert_frame_equal(reco, excepted.astype({Columns.Score: np.float32}))
 
-    reco = model.recommend_to_items(
-        target_items=[11, 12],
-        dataset=DATASET,
-        k=2,
-        items_to_recommend=[11, 12],
-        filter_itself=False,
-        add_rank_col=False,
-        return_external_ids=False,
+
+    @pytest.mark.parametrize(
+        "target_items, items_to_recommend",
+        (
+            (["i1", "i2"], [0, 1]),
+            ([0, 1], ["i1", "i2"]),
+            (["i1", "i2"], ["i1", "i2"]),
+        ),
     )
-
-    excepted = pd.DataFrame(
-        {
-            Columns.TargetItem: [0, 0, 1],
-            Columns.Item: [0, 1, 2],
-            Columns.Score: [0.1, 0.2, 0.3],
-        }
-    )
-
-    pd.testing.assert_frame_equal(reco, excepted)
+    def test_recommend_to_items_from_internal_ids_fails_when_not_integer_ids(
+        self, target_items: AnyIds, items_to_recommend: AnyIds
+    ) -> None:
+        with pytest.raises(TypeError):
+            self.model.recommend_to_items(
+                target_items=target_items,
+                dataset=DATASET,
+                k=2,
+                items_to_recommend=items_to_recommend,
+                assume_external_ids=False,
+            )
