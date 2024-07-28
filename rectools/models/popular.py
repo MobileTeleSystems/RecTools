6650cf7990627a91db719ea1dac116755d0fd7ad
Emiliy Feldman <feldlime@yandex.ru>
2024-07-28 13:12:37 +0200

    added config to popular model

--- rectools/models/popular.py
+++ rectools/models/popular.py
@@ -24,8 +24,11 @@ from tqdm.auto import tqdm
 
 from rectools import Columns, InternalIds
 from rectools.dataset import Dataset
+from rectools.models.base import ModelConfig
 from rectools.types import InternalIdsArray
 from rectools.utils import fast_isin_for_sorted_test_elements
+from pydantic import BaseModel
+import typing_extensions as tpe
 
 from .base import FixedColdRecoModelMixin, ModelBase, Scores, ScoresArray
 from .utils import get_viewed_item_ids
@@ -38,6 +41,14 @@ class Popularity(Enum):
     N_INTERACTIONS = "n_interactions"
     MEAN_WEIGHT = "mean_weight"
     SUM_WEIGHT = "sum_weight"
+
+
+class PopularModelConfig(ModelConfig):
+    popularity: Popularity = Popularity.N_USERS
+    period: tp.Optional[timedelta] = None
+    begin_from: tp.Optional[datetime] = None
+    add_cold: bool = False
+    inverse: bool = False
 
 
 class PopularModel(FixedColdRecoModelMixin, ModelBase):
