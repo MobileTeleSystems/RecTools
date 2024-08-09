187249acaa50e5b80d3ad6c32b5945c924b587c3
Daria Tikhonovich <daria.m.tikhonovich@gmail.com>
2024-08-09 23:57:31 +0300

    removed old tests

--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -19,7 +19,6 @@ import typing as tp
 import numpy as np
 import pandas as pd
 import pytest
-from pytest_mock import MockerFixture
 
 from rectools import Columns
 from rectools.dataset import Dataset
@@ -31,7 +30,7 @@ from rectools.models.base import (
     Scores,
     SemiInternalRecoTriplet,
 )
-from rectools.types import AnyIds, ExternalIds, InternalIds
+from rectools.types import ExternalIds, InternalIds
 
 from .data import DATASET, INTERACTIONS
 
@@ -80,135 +79,6 @@ def test_raise_when_k_is_not_positive_i2i(k: int) -> None:
             dataset=DATASET,
             k=k,
         )
-
-
-class TestRecommendWithInternalIds:
-    def setup_method(self) -> None:
-        class SomeModel(ModelBase):
-            def _fit(self, dataset: Dataset, *args: tp.Any, **kwargs: tp.Any) -> None:
-                pass
-
-            def _recommend_u2i(
-                self,
-                user_ids: np.ndarray,
-                dataset: Dataset,
-                k: int,
-                filter_viewed: bool,
-                sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
-            ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
-                return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
-
-            def _recommend_i2i(
-                self,
-                target_ids: np.ndarray,
-                dataset: Dataset,
-                k: int,
-                sorted_item_ids_to_recommend: tp.Optional[np.ndarray],
-            ) -> tp.Tuple[InternalIds, InternalIds, Scores]:
-                return [0, 0, 1], [0, 1, 2], [0.1, 0.2, 0.3]
-
-        self.model = SomeModel().fit(DATASET)
-
-    def test_u2i_success(self, mocker: MockerFixture) -> None:
-        model = self.model
-        users = [0, 1]
-        items_to_recommend = [0, 1, 2]
-
-        spy = mocker.spy(model, "_recommend_u2i")
-        reco = model.recommend(
-            users=users,
-            dataset=DATASET,
-            k=2,
-            filter_viewed=False,
-            items_to_recommend=items_to_recommend,
-            assume_external_ids=False,
-            add_rank_col=False,
-        )
-
-        args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
-        assert list(args[0]) == users
-        assert list(args[4]) == items_to_recommend
-
-        excepted = pd.DataFrame(
-            {
-                Columns.User: [0, 0, 1],
-                Columns.Item: [0, 1, 2],
-                Columns.Score: [0.1, 0.2, 0.3],
-            }
-        )
-        pd.testing.assert_frame_equal(reco, excepted.astype({Columns.Score: np.float32}))
-
-    @pytest.mark.parametrize(
-        "users, items_to_recommend, error_type",
-        (
-            (["u1", "u2"], [0, 1], TypeError),
-            ([0, 1], ["i1", "i2"], TypeError),
-            (["u1", "u2"], ["i1", "i2"], TypeError),
-            ([0, 1], [-1, 1], ValueError),
-            ([-1, 1], [0, 1], ValueError),
-        ),
-    )
-    def test_u2i_with_incorrect_ids(self, users: AnyIds, items_to_recommend: AnyIds, error_type: tp.Type) -> None:
-        with pytest.raises(error_type):
-            self.model.recommend(
-                users=users,
-                dataset=DATASET,
-                k=2,
-                filter_viewed=False,
-                items_to_recommend=items_to_recommend,
-                assume_external_ids=False,
-            )
-
-    def test_i2i_success(self, mocker: MockerFixture) -> None:
-        model = self.model
-        target_items = [0, 1, 2]
-        items_to_recommend = [0, 1, 2]
-
-        spy = mocker.spy(model, "_recommend_i2i")
-        reco = model.recommend_to_items(
-            target_items=target_items,
-            dataset=DATASET,
-            k=2,
-            items_to_recommend=items_to_recommend,
-            assume_external_ids=False,
-            add_rank_col=False,
-            filter_itself=False,
-        )
-
-        args, _ = spy.call_args  # args and kwargs properties are unavailable in Python < 3.8
-        assert list(args[0]) == target_items
-        assert list(args[3]) == items_to_recommend
-
-        excepted = pd.DataFrame(
-            {
-                Columns.TargetItem: [0, 0, 1],
-                Columns.Item: [0, 1, 2],
-                Columns.Score: [0.1, 0.2, 0.3],
-            }
-        )
-        pd.testing.assert_frame_equal(reco, excepted.astype({Columns.Score: np.float32}))
-
-    @pytest.mark.parametrize(
-        "target_items, items_to_recommend, error_type",
-        (
-            (["i1", "i2"], [0, 1], TypeError),
-            ([0, 1], ["i1", "i2"], TypeError),
-            (["i1", "i2"], ["i1", "i2"], TypeError),
-            ([0, 1], [-1, 1], ValueError),
-            ([-1, 1], [0, 1], ValueError),
-        ),
-    )
-    def test_i2i_with_incorrect_ids(
-        self, target_items: AnyIds, items_to_recommend: AnyIds, error_type: tp.Type
-    ) -> None:
-        with pytest.raises(error_type):
-            self.model.recommend_to_items(
-                target_items=target_items,
-                dataset=DATASET,
-                k=2,
-                items_to_recommend=items_to_recommend,
-                assume_external_ids=False,
-            )
 
 
 class TestHotWarmCold:
