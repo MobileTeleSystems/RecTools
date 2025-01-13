d25af1ee5687ef2ee49ac76b39462e3a9adc70d2
blondered <nykenott@gmail.com>
2025-01-13 15:09:33 +0300

    use_gpu_ranking parametrize for dssm tests

--- tests/models/test_dssm.py
+++ tests/models/test_dssm.py
@@ -17,6 +17,7 @@ import typing as tp
 import numpy as np
 import pandas as pd
 import pytest
+from implicit.gpu import HAS_CUDA
 from lightning_fabric import seed_everything
 
 from rectools.columns import Columns
@@ -32,6 +33,7 @@ from .data import INTERACTIONS
 
 @pytest.mark.filterwarnings("ignore::pytorch_lightning.utilities.warnings.PossibleUserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning")
+@pytest.mark.parametrize("recommend_use_gpu_ranking", (False, True) if HAS_CUDA else (False,))
 class TestDSSMModel:
     def setup_method(self) -> None:
         self._seed_everything()
@@ -91,9 +93,9 @@ class TestDSSMModel:
                 True,
                 pd.DataFrame(
                     {
-                        Columns.User: [10, 10, 10, 20, 20, 20, 50, 50, 50],
-                        Columns.Item: [13, 15, 16, 15, 16, 17, 11, 12, 15],
-                        Columns.Rank: [1, 2, 3, 1, 2, 3, 1, 2, 3],
+                        Columns.User: [10, 10, 20, 20, 50, 50],
+                        Columns.Item: [13, 15, 15, 17, 11, 12],
+                        Columns.Rank: [1, 2, 1, 2, 1, 2],
                     }
                 ),
             ),
@@ -101,16 +103,23 @@ class TestDSSMModel:
                 False,
                 pd.DataFrame(
                     {
-                        Columns.User: [10, 10, 10, 20, 20, 20, 50, 50, 50],
-                        Columns.Item: [12, 11, 13, 11, 12, 13, 11, 12, 15],
-                        Columns.Rank: [1, 2, 3, 1, 2, 3, 1, 2, 3],
+                        Columns.User: [10, 10, 20, 20, 50, 50],
+                        Columns.Item: [12, 11, 11, 12, 11, 12],
+                        Columns.Rank: [1, 2, 1, 2, 1, 2],
                     }
                 ),
             ),
         ),
     )
     @pytest.mark.parametrize("default_base_model", (True, False))
-    def test_u2i(self, dataset: Dataset, filter_viewed: bool, expected: pd.DataFrame, default_base_model: bool) -> None:
+    def test_u2i(
+        self,
+        dataset: Dataset,
+        filter_viewed: bool,
+        expected: pd.DataFrame,
+        default_base_model: bool,
+        recommend_use_gpu_ranking: bool,
+    ) -> None:
         if default_base_model:
             base_model = None
         else:
@@ -127,10 +136,11 @@ class TestDSSMModel:
             max_epochs=3,
             batch_size=4,
             deterministic=True,
+            recommend_use_gpu_ranking=recommend_use_gpu_ranking,
         )
         model.fit(dataset=dataset, dataset_valid=dataset)
         users = np.array([10, 20, 50])
-        actual = model.recommend(users=users, dataset=dataset, k=3, filter_viewed=filter_viewed)
+        actual = model.recommend(users=users, dataset=dataset, k=2, filter_viewed=filter_viewed)
         pd.testing.assert_frame_equal(actual.drop(columns=Columns.Score), expected)
         pd.testing.assert_frame_equal(
             actual.sort_values([Columns.User, Columns.Score], ascending=[True, True]).reset_index(drop=True),
@@ -162,12 +172,15 @@ class TestDSSMModel:
             ),
         ),
     )
-    def test_with_whitelist(self, dataset: Dataset, filter_viewed: bool, expected: pd.DataFrame) -> None:
+    def test_with_whitelist(
+        self, dataset: Dataset, filter_viewed: bool, expected: pd.DataFrame, recommend_use_gpu_ranking: bool
+    ) -> None:
         model = DSSMModel(
             n_factors=32,
             max_epochs=3,
             batch_size=4,
             deterministic=True,
+            recommend_use_gpu_ranking=recommend_use_gpu_ranking,
         )
         model.fit(dataset=dataset)
         users = np.array([10, 50])
@@ -184,7 +197,7 @@ class TestDSSMModel:
             actual,
         )
 
-    def test_get_vectors(self, dataset: Dataset) -> None:
+    def test_get_vectors(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         base_model = DSSM(
             n_factors_item=32,
             n_factors_user=32,
@@ -200,6 +213,7 @@ class TestDSSMModel:
             batch_size=4,
             dataloader_num_workers=0,
             callbacks=None,
+            recommend_use_gpu_ranking=recommend_use_gpu_ranking,
         )
         model.fit(dataset=dataset)
         user_embeddings, item_embeddings = model.get_vectors(dataset)
@@ -221,7 +235,7 @@ class TestDSSMModel:
         np.testing.assert_equal(vectors_reco, reco_item_ids)
         np.testing.assert_almost_equal(vectors_scores, reco_scores, decimal=5)
 
-    def test_raises_when_get_vectors_from_not_fitted(self, dataset: Dataset) -> None:
+    def test_raises_when_get_vectors_from_not_fitted(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         base_model = DSSM(
             n_factors_item=32,
             n_factors_user=32,
@@ -249,9 +263,9 @@ class TestDSSMModel:
                 None,
                 pd.DataFrame(
                     {
-                        Columns.TargetItem: [11, 11, 11, 12, 12, 12, 16, 16, 16],
-                        Columns.Item: [11, 15, 12, 12, 13, 11, 16, 17, 15],
-                        Columns.Rank: [1, 2, 3, 1, 2, 3, 1, 2, 3],
+                        Columns.TargetItem: [11, 11, 12, 12],
+                        Columns.Item: [11, 15, 12, 13],
+                        Columns.Rank: [1, 2, 1, 2],
                     }
                 ),
             ),
@@ -260,9 +274,9 @@ class TestDSSMModel:
                 None,
                 pd.DataFrame(
                     {
-                        Columns.TargetItem: [11, 11, 11, 12, 12, 12, 16, 16, 16],
-                        Columns.Item: [15, 12, 16, 13, 11, 15, 17, 15, 14],
-                        Columns.Rank: [1, 2, 3, 1, 2, 3, 1, 2, 3],
+                        Columns.TargetItem: [11, 11, 12, 12],
+                        Columns.Item: [15, 12, 13, 11],
+                        Columns.Rank: [1, 2, 1, 2],
                     }
                 ),
             ),
@@ -271,29 +285,35 @@ class TestDSSMModel:
                 np.array([11, 15, 12]),
                 pd.DataFrame(
                     {
-                        Columns.TargetItem: [11, 11, 12, 12, 16, 16, 16],
-                        Columns.Item: [15, 12, 11, 15, 15, 11, 12],
-                        Columns.Rank: [1, 2, 1, 2, 1, 2, 3],
+                        Columns.TargetItem: [11, 11, 12, 12],
+                        Columns.Item: [15, 12, 11, 15],
+                        Columns.Rank: [1, 2, 1, 2],
                     }
                 ),
             ),
         ),
     )
     def test_i2i(
-        self, dataset: Dataset, filter_itself: bool, whitelist: tp.Optional[np.ndarray], expected: pd.DataFrame
+        self,
+        dataset: Dataset,
+        filter_itself: bool,
+        whitelist: tp.Optional[np.ndarray],
+        expected: pd.DataFrame,
+        recommend_use_gpu_ranking: bool,
     ) -> None:
         model = DSSMModel(
             n_factors=10,
             max_epochs=3,
             batch_size=4,
             deterministic=True,
+            recommend_use_gpu_ranking=recommend_use_gpu_ranking,
         )
         model.fit(dataset=dataset, dataset_valid=dataset)
-        target_items = np.array([11, 12, 16])
+        target_items = np.array([11, 12])
         actual = model.recommend_to_items(
             target_items=target_items,
             dataset=dataset,
-            k=3,
+            k=2,
             filter_itself=filter_itself,
             items_to_recommend=whitelist,
         )
@@ -303,7 +323,7 @@ class TestDSSMModel:
             actual,
         )
 
-    def test_u2i_with_cold_users(self, dataset: Dataset) -> None:
+    def test_u2i_with_cold_users(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         model = DSSMModel().fit(dataset)
         with pytest.raises(ValueError, match="doesn't support recommendations for cold users"):
             model.recommend(
@@ -313,7 +333,7 @@ class TestDSSMModel:
                 filter_viewed=False,
             )
 
-    def test_i2i_with_cold_items(self, dataset: Dataset) -> None:
+    def test_i2i_with_cold_items(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         model = DSSMModel().fit(dataset)
         with pytest.raises(ValueError, match="doesn't support recommendations for cold items"):
             model.recommend_to_items(
@@ -323,7 +343,9 @@ class TestDSSMModel:
             )
 
     @pytest.mark.parametrize("exclude_features", ("user", "item"))
-    def test_raises_when_no_features_in_dataset(self, dataset: Dataset, exclude_features: str) -> None:
+    def test_raises_when_no_features_in_dataset(
+        self, dataset: Dataset, exclude_features: str, recommend_use_gpu_ranking: bool
+    ) -> None:
         dataset = Dataset(
             dataset.user_id_map,
             dataset.item_id_map,
@@ -335,11 +357,11 @@ class TestDSSMModel:
         with pytest.raises(ValueError, match="requires user and item features"):
             model.fit(dataset)
 
-    def test_second_fit_refits_model(self, dataset: Dataset) -> None:
+    def test_second_fit_refits_model(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         model = DSSMModel(deterministic=True)
         assert_second_fit_refits_model(model, dataset, pre_fit_callback=self._seed_everything)
 
-    def test_dumps_loads(self, dataset: Dataset) -> None:
+    def test_dumps_loads(self, dataset: Dataset, recommend_use_gpu_ranking: bool) -> None:
         model = DSSMModel()
         model.fit(dataset)
         assert_dumps_loads_do_not_change_model(model, dataset, check_configs=False)
