d37c6466d8a0ee191dabeb0e8acd26dc67afbf8c
spirinamayya <maspirina@edu.hse.ru>
2025-01-16 15:42:45 +0300

    removed sorting sessions by users in train

--- rectools/models/nn/transformer_data_preparator.py
+++ rectools/models/nn/transformer_data_preparator.py
@@ -56,6 +56,7 @@ class SequenceDataset(TorchDataset):
     def from_interactions(
         cls,
         interactions: pd.DataFrame,
+        sort_users: bool = False,
     ) -> "SequenceDataset":
         """
         Group interactions by user.
@@ -66,9 +67,13 @@ class SequenceDataset(TorchDataset):
         interactions : pd.DataFrame
             User-item interactions.
         """
+        # Recommend dataloader should return interactions sorted by user ids.
+        # User ids here are internal user ids in dataset.interactions.df that was prepared for recommendations.
+        # Sorting sessions by user ids will ensure that these ids will also be correct indexes in user embeddings matrix
+        # that will be returned by the net.
         sessions = (
             interactions.sort_values(Columns.Datetime)
-            .groupby(Columns.User, sort=True)[[Columns.Item, Columns.Weight]]
+            .groupby(Columns.User, sort=sort_users)[[Columns.Item, Columns.Weight]]
             .agg(list)
         )
         sessions, weights = (
@@ -211,7 +216,7 @@ class SessionEncoderDataPreparatorBase:
 
     def get_dataloader_recommend(self, dataset: Dataset) -> DataLoader:
         """TODO"""
-        sequence_dataset = SequenceDataset.from_interactions(dataset.interactions.df)
+        sequence_dataset = SequenceDataset.from_interactions(interactions=dataset.interactions.df, sort_users=True)
         recommend_dataloader = DataLoader(
             sequence_dataset,
             batch_size=self.batch_size,
