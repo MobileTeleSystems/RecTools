44be2d65ca9920befbd28b63bbd68e7609cb940e
Daria Tikhonovich <daria.m.tikhonovich@gmail.com>
2025-01-27 14:26:23 +0300

    pyling

--- tests/models/nn/test_sasrec.py
+++ tests/models/nn/test_sasrec.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# pylint: ignore = too-many-lines
+
 import os
 import typing as tp
 from functools import partial
