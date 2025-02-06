e7ec51c525369d9ecb175f21d6401126de40c6d4
Daria <93913290+blondered@users.noreply.github.com>
2025-02-06 22:38:04 +0300

    Fix/checkpoints (#259)
    Checkpoints were failing if `get_trainer_func` was not None.

--- tests/models/nn/test_transformer_base.py
+++ tests/models/nn/test_transformer_base.py
@@ -20,7 +20,6 @@ import pandas as pd
 import pytest
 import torch
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import CSVLogger
 
 from rectools import Columns
@@ -31,7 +30,7 @@ from rectools.models.nn.transformer_base import TransformerModelBase
 from tests.models.utils import assert_save_load_do_not_change_model
 
 from ..data import INTERACTIONS
-from .utils import custom_trainer, leave_one_out_mask
+from .utils import custom_trainer, custom_trainer_ckpt, leave_one_out_mask
 
 
 class TestTransformerModelBase:
@@ -162,20 +161,13 @@ class TestTransformerModelBase:
         tmp_path: str,
         dataset_item_features: Dataset,
     ) -> None:
+
         model = model_cls.from_config(
             {
                 "deterministic": True,
-                "item_net_block_types": (IdEmbeddingsItemNet, CatFeaturesItemNet),
+                "item_net_block_types": (IdEmbeddingsItemNet,),  # TODO: add CatFeaturesItemNet
+                "get_trainer_func": custom_trainer_ckpt,
             }
-        )
-        model._trainer = Trainer(  # pylint: disable=protected-access
-            default_root_dir=tmp_path,
-            max_epochs=2,
-            min_epochs=2,
-            deterministic=True,
-            accelerator="cpu",
-            devices=1,
-            callbacks=ModelCheckpoint(filename="last_epoch"),
         )
         model.fit(dataset_item_features)
 
