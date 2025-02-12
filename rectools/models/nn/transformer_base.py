eb0daaeaf1206403cbca0a41d442a8f981498256
spirinamayya <maspirina@edu.hse.ru>
2025-02-12 18:12:12 +0300

    added test for load_weights_from_checkpoint

--- rectools/models/nn/transformer_base.py
+++ rectools/models/nn/transformer_base.py
@@ -504,10 +504,8 @@ class TransformerModelBase(ModelBase[TransformerModelConfig_T]):  # pylint: disa
         checkpoint = torch.load(checkpoint_path, weights_only=False)
         loaded = cls._model_from_checkpoint(checkpoint)
         return loaded
-    
-    def load_weights_from_checkpoint(self, checkpoint_path: tp.Union[str, Path]):
+
+    def load_weights_from_checkpoint(self, checkpoint_path: tp.Union[str, Path]) -> None:
         """Load modelweights from Lightning checkpoint path."""
-        checkpoint = torch.load(checkpoint_path, weights_only=True)
-        self.lightning_model.load_state_dict(checkpoint[''])
-
-
+        checkpoint = torch.load(checkpoint_path, weights_only=False)
+        self.lightning_model.load_state_dict(checkpoint["state_dict"])
