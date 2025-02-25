61815a345bf6a8d221f044fb4df479c0e9d815b1
Olesya Bulgakova <oirvach@edu.hse.ru>
2025-02-25 17:06:01 +0300

    fix compat.py

--- rectools/compat.py
+++ rectools/compat.py
@@ -86,9 +86,3 @@ class MetricsApp(RequirementUnavailable):
     """Dummy class, which is returned if there are no dependencies required for the model"""
 
     requirement = "visuals"
-
-
-class CatBoostReranker(RequirementUnavailable):
-    """Dummy class, which is returned if there are no dependencies required for the model"""
-
-    requirement = "catboost"
