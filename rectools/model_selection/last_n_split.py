0a6b8dc7cf0362fe0d356a5b0729f68611d64cb0
Aleksey Kuzin <kuzinmails@yandex.ru>
2025-06-11 15:53:48 +0300

    spliter v2

--- rectools/model_selection/last_n_split.py
+++ rectools/model_selection/last_n_split.py
@@ -1,27 +1,12 @@
-#  Copyright 2023-2024 MTS (Mobile Telesystems)
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-"""LastNSplitter."""
-
 import typing as tp
 
 import numpy as np
 import pandas as pd
-
 from rectools import Columns
 from rectools.dataset import Interactions
 from rectools.model_selection.splitter import Splitter
+
+# Rectools splitter but it considers last interaction as last appeared in dataframe when datetime is not unique
 
 
 class LastNSplitter(Splitter):
@@ -104,7 +89,13 @@ class LastNSplitter(Splitter):
         idx = pd.RangeIndex(0, len(df))
 
         # last event - rank=1
-        inv_ranks = df.groupby(Columns.User)[Columns.Datetime].rank(method="first", ascending=False)
+        time_order = (
+            df.groupby("user_id")[Columns.Datetime]
+            .rank(method="first", ascending=True)
+            .astype(int)
+        )
+        n_interactions = df.groupby("user_id").transform("size").astype(int)
+        inv_ranks = n_interactions - time_order + 1
 
         for i_split in range(self.n_splits)[::-1]:
             min_rank = i_split * self.n  # excluded
